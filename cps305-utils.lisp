3155de6eb1245fc8d13c3362211ec33f78e3c0b4
Marcus Santos <34948880+marcus3santos@users.noreply.github.com>
2021-12-20 16:51:07 -0500

    Add files via upload

---
+++ cps305-utils.lisp
@@ -0,0 +1,30 @@
+;;;; cps305-utils.lisp
+
+(in-package #:cps305-utils)
+
+;; Unit test Macros
+
+(defvar *test-name* nil)
+
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ...~a: ~a~%" result *test-name* form)
+  result)
+
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
+(defmacro combine-results (&body forms)
+  (with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+(defmacro check (&body forms)
+  `(combine-results
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defmacro deftest (name parameters &body body)
+  `(defun ,name ,parameters
+     (let ((*test-name* (append *test-name* (list ',name))))
+       ,@body)))
---
+++ package.lisp
@@ -0,0 +1,4 @@
+;;;; package.lisp
+
+(defpackage #:cps305-utils
+  (:use #:cl))
